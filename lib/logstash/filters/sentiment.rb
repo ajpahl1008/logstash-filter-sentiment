e107a81c458af5265aa69c5cd4ed6e47d7a3e146
AJ Pahl <ajpahl1008@gmail.com>
2018-01-21 13:56:13 -0500

    Added some output to logs.  And a configuration element target_field which defaults to message

--- lib/logstash/filters/sentiment.rb
+++ lib/logstash/filters/sentiment.rb
@@ -11,7 +11,7 @@ class LogStash::Filters::Sentiment < LogStash::Filters::Base
 
   config_name "sentiment"
   # Replace the message with this value.
-  # config :message, :validate => :string, :default => "HelloWorld!"
+  config :target_field, :validate => :string, :default => "message"
 
   public
   def register
@@ -21,16 +21,20 @@ class LogStash::Filters::Sentiment < LogStash::Filters::Base
   public
   def filter(event)
 
-    @logger.debug? && @logger.debug("Conducting Sentiment Analysis on Event String: #{event.get("message")}")
+    if !event.get(target_field).nil?
+       @logger.debug? && @logger.debug("Conducting Sentiment Analysis on Field: #{target_field}")
 
-    analyzer = Sentimental.new
-    analyzer.load_defaults
-    sentiment = analyzer.sentiment event.get("message")
-    score = analyzer.score event.get("message")
-    @logger.debug? && @logger.debug(puts sentiment, score)
+       analyzer = Sentimental.new
+       analyzer.load_defaults
+       sentiment = analyzer.sentiment event.get(target_field)
+       score = analyzer.score event.get(target_field)
+       @logger.debug? && @logger.debug(puts sentiment, score)
 
-    event.set("sentiment", sentiment)
-    event.set("sentiment_score", score)
+       event.set("sentiment", sentiment)
+       event.set("sentiment_score", score)
+    else
+      puts "logstash-filter-sentiment: WARNING: target_field '#{target_field}' does not exist in event"
+    end
 
     # filter_matched should go in the last line of our successful code
     filter_matched(event)
